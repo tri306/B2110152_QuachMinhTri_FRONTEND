9bb2ff206aa18b97b3f3fc6730b73dbadc5daca8
Quach Minh Tri <triquach543@gmail.com>
2024-04-04 02:25:28 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,18 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  server: {
-    port:3001,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-})
+  server: {
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    }
+  },
+});
